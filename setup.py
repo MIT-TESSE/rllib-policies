c1e60406614b851c748fd92f1f42ad7dacba44b9
Zac Ravichandran <zachary.ravichandran@ll.mit.edu>
2022-01-31 15:12:36 -0500

    Update setup.py

--- setup.py
+++ setup.py
@@ -6,5 +6,5 @@ setup(
     packages=find_packages("src"),
     python_requires=">=3.7",
     package_dir={"": "src"},
-    install_requires=["numpy>=1.17.3", "ray[rllib]>0.8.0", "torch>=1.4.0"],
+    install_requires=["numpy<=1.19", "ray[rllib]>0.8.0", "torch>=1.4.0"],
 )
